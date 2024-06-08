e0f0f425a1d8fc7ff7284f2462802fc152e1f6d3
developerabdulmamun <abdulmamun.dev@gmail.com>
2024-06-08 10:25:13 +0600

    Update grid layout for home page

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import AdditionalCharges from "@/components/AdditionalCharges";
+import ChargesSummary from "@/components/ChargesSummary";
 import CustomerInfo from "@/components/CustomerInfo";
 import ReservationDetails from "@/components/ReservationDetails";
 import VehicleInfo from "@/components/VehicleInfo";
@@ -14,15 +15,19 @@ export default function Home() {
           </button>
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-          <div className="flex flex-col gap-6">
+        <div className="grid grid-cols-3 lg:grid-cols-10 gap-6">
+          <div className="col-span-4 lg:col-span-3 space-y-6">
             <ReservationDetails />
             <VehicleInfo />
           </div>
 
-          <div className="flex flex-col gap-6">
+          <div className="col-span-4 lg:col-span-3 space-y-6">
             <CustomerInfo />
             <AdditionalCharges />
+          </div>
+
+          <div className="col-span-4 lg:col-span-4">
+            <ChargesSummary />
           </div>
         </div>
       </div>
---
+++ components/ChargesSummary.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SectionHeader from "./shared/SectionHeader";
+
+const ChargesSummary = () => {
+  return (
+    <div>
+      <SectionHeader title={"Charges Summary"} />
+    </div>
+  );
+};
+
+export default ChargesSummary;
